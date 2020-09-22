ee749a4c584dc6bfb69d18c0e8c6207e84c52b3d
sevenman7 <manuem07@ucm.es>
2020-09-22 17:39:57 +0200

    Update C2.java

--- src/main/java/es/upm/miw/iwvg_devops/git/C2.java
+++ src/main/java/es/upm/miw/iwvg_devops/git/C2.java
@@ -2,10 +2,6 @@ package es.upm.miw.iwvg_devops.git;
 
 public class C2 {
     public String fiveHoursWithManu() {
-        return "I cant bear more";
-    }
-
-    public String fiveHoursMoreWithManu() {
-        return "Help me, please";
+        return " ";
     }
 }
